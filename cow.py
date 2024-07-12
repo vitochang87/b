86a5b73ca8439bfcd2c33f7c16f32bccd56d8b8b
vitochang87 <daptoper657@gmail.com>
2024-07-12 15:14:02 +0000

    cow ai

--- cow.py
+++ cow.py
@@ -2,7 +2,6 @@ import streamlit as st
 from langchain_openai import ChatOpenAI  # pip install -U langchain-openai
 from langchain_core.messages import HumanMessage, AIMessage  # pip install langchain
 
-
 col1, col2 = st.columns([4, 1])
 with col1:
     openai_key = st.text_input("Password", type="password", key="password")
@@ -13,11 +12,10 @@ with col1:
 if "history" not in st.session_state:  # 初始化對話紀錄
     st.session_state.history = []  # 對話紀錄
 
-
 with col2:
     if st.button("🗑️"):  # 清空對話紀錄
         st.session_state.history = []  # 對話紀錄清空
-        st.rerun()  # 重新整理頁面
+        st.experimental_rerun()  # 重新整理頁面
 
 msgs = [
     HumanMessage(
@@ -27,7 +25,6 @@ msgs = [
 for actor, message in st.session_state.history:  # 透過對話紀錄來初始化對話訊息列表
     if actor == "user":  # 如果是使用者的訊息
         st.chat_message("user", avatar="🦖").write(message)  # 顯示使用者的對話紀錄訊息
-        # st.chat_message("user", avatar=st.image("image/蘋果.jpg")).write(message)
         msgs.append(HumanMessage(message))  # 將使用者的訊息加入對話訊息列表
     else:
         st.chat_message("assistant", avatar="🐮").write(
@@ -35,7 +32,9 @@ for actor, message in st.session_state.history:  # 透過對話紀錄來初始�
         )  # 顯示AI回傳的對話紀錄訊息
         msgs.append(AIMessage(message))  # 將AI回傳的訊息加入對話訊息列表
 
-chat = ChatOpenAI(model="gpt-4o", temperature=0.2, api_key=openai_key)
+chat = ChatOpenAI(
+    model="gpt-4", temperature=0.2, api_key=openai_key
+)  # 確認模型名稱是否正確
 prompt = st.chat_input("請輸入想要對話的訊息")  # 顯示對話輸入框
 if prompt:  # 如果使用者輸入了訊息
     st.session_state.history.append(["user", prompt])  # 將使用者輸入的訊息加入對話紀錄
@@ -44,4 +43,4 @@ if prompt:  # 如果使用者輸入了訊息
     st.session_state.history.append(
         ["assistant", response]
     )  # 將ChatOpenAI回傳的訊息加入對話紀錄
-    st.rerun()
+    st.experimental_rerun()  # 重新整理頁面
